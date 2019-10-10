7913f9344fcc9470f8701cad0032d793705ff59c
iaroslav <iaroslav@tonpolo-MacBook-Pro.local>
2019-10-10 13:14:59 +0300

    Done 3.client

---
+++ lesson_3/client.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+parser = ArgumentParser()
+parser.add_argument('--host', help='client address', type=str, default='')
+parser.add_argument('--port', help='client port', type=int, default=7777)
+args = parser.parse_args()
+
+
+with socket(AF_INET, SOCK_STREAM) as client:
+    client.connect((args.host, args.port))
+    client.sendall('Hello, server!'.encode(encoding='utf-8'))
+    data = client.recv(1024)
+    print(f'message: {data.decode(encoding="utf-8")} length: {len(data)}')
