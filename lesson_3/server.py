00453998e423c3ec6098835a920050e5b2f89efb
iaroslav <iaroslav@tonpolo-MacBook-Pro.local>
2019-10-10 13:14:38 +0300

    Done 3.server

---
+++ lesson_3/server.py
@@ -0,0 +1,18 @@
+from socket import socket, AF_INET, SOCK_STREAM
+
+HOST = ''    # All available interfaces
+PORT = 7777  # Non-privileged port
+
+
+with socket(AF_INET, SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    server.listen(1)
+    connection, address = server.accept()
+
+    with connection:
+        while True:
+            data = connection.recv(1024)  # bufsize=1024B
+            if not data:
+                break
+            print(f'message: {data.decode(encoding="utf-8")} client: {address}')
+            connection.sendall('Hello, client!'.encode(encoding='utf-8'))
